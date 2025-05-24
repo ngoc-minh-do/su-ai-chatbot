dd0a3340f3fb6ef1f0f91097346faa74172f700d
ngoc <ngochust56@gmail.com>
2025-05-24 16:06:54 +0900

    custom chat interface

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import gradio as gr
 
 
-def generate_response(prompt_txt):
-    return prompt_txt
+def response_fn(message, history):
+    return message
 
 
-chat_application = gr.Interface(
-    fn=generate_response,
-    allow_flagging="never",
-    inputs=gr.Textbox(label="Input", lines=2, placeholder="Type your question here..."),
-    outputs=gr.Textbox(label="Output"),
-    title="Su AI Chatbot",
-    description="Ask any question and the chatbot will try to answer.",
+demo = gr.ChatInterface(
+    response_fn,
+    type="messages",
+    save_history=True,
+    css="footer{display:none !important}",
 )
 
-# Launch the app
-chat_application.launch(server_name="0.0.0.0", server_port=7860)
+if __name__ == "__main__":
+    demo.launch(server_name="0.0.0.0", server_port=7860)
